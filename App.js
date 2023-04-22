0a7bb920914cbe5ea9fc477bb08e7be687bb5c35
Sunit Shirke <sunitshirke88@gmail.com>
2023-04-22 06:55:01 +0530

    refactor: text input and container

--- App.js
+++ App.js
@@ -2,7 +2,14 @@ import { gql } from "@apollo/client";
 import { NhostClient } from "@nhost/nhost-js";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const nhost = new NhostClient({
   subdomain: "wwgsiqjwetcrvgptnyta",
@@ -11,7 +18,7 @@ const nhost = new NhostClient({
 
 export default function App() {
   const [patternList, setPatternList] = useState([]);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const PATTERNS = gql`
     query {
       patterns {
@@ -28,10 +35,12 @@ export default function App() {
     if (text.length < 3) {
       setPatternList(patternList);
       return;
-    } 
-    const newList = patternList.filter((p) => p.name.toLowerCase().includes(text.toLowerCase()));
-    setPatternList(newList)
-  }
+    }
+    const newList = patternList.filter((p) =>
+      p.name.toLowerCase().includes(text.toLowerCase())
+    );
+    setPatternList(newList);
+  };
 
   console.log("sfsfsf");
   const url = nhost.graphql.getUrl();
@@ -51,23 +60,25 @@ export default function App() {
   return (
     // <NhostReactProvider nhost={nhost}>
     <View style={styles.container}>
-          <TextInput
-        style={{padding: 5, width: "100%", fontSize: 16.0, borderRadius: 4, borderWidth: 2, borderColor: "#000", backgroundColor: "#888", marginVertical: 5}}
-        placeholder="Type here to translate!"
-        onChangeText={newText => setText(newText)}
+      <TextInput
+        selectionColor={"#000"}
+        underlineColorAndroid={"#000"}
+        style={styles.textInput}
+        placeholder="find your match"
+        onChangeText={(newText) => setText(newText)}
         defaultValue={text}
         onChange={handleSearch}
-        
       />
       {/* <ActivityIndicator size="large" /> */}
       <FlatList
         data={patternList}
-        renderItem={
-          ({ item }) => 
-          <TouchableOpacity onPress={() => console.log(item.name) }>
-          <Text style={styles.item}>{item.name} : {item.preview}</Text>
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => console.log(item.name)}>
+            <Text style={styles.item}>
+              {item.name} : {item.preview}
+            </Text>
           </TouchableOpacity>
-          }
+        )}
       />
       <StatusBar style="auto" />
     </View>
@@ -79,11 +90,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 22,
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
   },
   item: {
-    padding: 10,
     fontSize: 18,
     fontWeight: "500",
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
+  },
+  textInput: {
+    padding: 12,
+    width: "100%",
+    fontSize: 18.0,
+    backgroundColor: "#fff",
+    marginVertical: 5,
+    height: 75,
+    width: 250,
+    marginVertical: 10
   },
 });
