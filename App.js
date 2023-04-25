779198a09731e5516e5f277494d11933c516e2ed
Sunit Shirke <sunitshirke88@gmail.com>
2023-04-25 23:56:07 +0530

    solves #5

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { NhostClient } from "@nhost/nhost-js";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
@@ -8,7 +9,7 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  View,
+  View
 } from "react-native";
 import Card from "./src/components/card";
 
@@ -62,7 +63,24 @@ export default function App() {
   return (
     // <NhostReactProvider nhost={nhost}>
     <View className="flex-1 items-center justify-center bg-white">
-      <TextInput
+                <Text className="text-center text-xl w-36">
+              microregex
+            </Text>
+    <View className="flex bg-[#00000014] justify-center flex-row items-center mt-10 mb-4 rounded-xl px-4 focus:bg-white focus:border-2 focus:border-[#1967d2]">
+    <Ionicons color="#0000008c" selectionColor="#1967d2" name="ios-search" size={24} className="m-2" />
+    <TextInput
+            selectionColor={"#0000008c"}
+        underlineColorAndroid="transparent"
+        className="px-2 py-2 w-56 font-semibold text-lg text-[##0000008c] focus:bg-white focus:text-[##0000008c]"
+        placeholder="find your match"
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+        onChange={handleSearch}
+        color
+    />
+    <MaterialIcons name="cancel" size={24} color="#0000008c" onPress={() => console.log("pressed")} />
+</View>
+      {/* <TextInput
         selectionColor={"#000"}
         underlineColorAndroid={"#000"}
         className="px-2 py-4 w-56 font-medium text-xl bg-white mt-8 mb-4"
@@ -70,7 +88,7 @@ export default function App() {
         onChangeText={(newText) => setText(newText)}
         defaultValue={text}
         onChange={handleSearch}
-      />
+      /> */}
       {/* <ActivityIndicator size="large" /> */}
       <FlatList
         data={patternList}
@@ -99,5 +117,23 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  searchSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+},
+searchIcon: {
+    padding: 10,
+},
+textInput: {
+    flex: 1,
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    backgroundColor: '#fff',
+    color: '#424242',
+},
 });
